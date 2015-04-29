44db9fd67232086c31b351d6fe3b1b1c6c3aa777
nvrionis <nick.vrionis@gmail.com>
2015-04-29 12:58:06 +0300

    Change so storage is preselected if only one

--- ember_django/frontend/app/controllers/cluster_create.js
+++ ember_django/frontend/app/controllers/cluster_create.js
@@ -680,7 +680,10 @@ App.ClusterCreateController = Ember.Controller.extend({
 	storage_buttons : function() {
 		var elements = document.getElementsByName("storage_button");
 		var length = elements.length;
-		if (length == 1) {elements[0].style.color = "white";}
+		if (length == 1) {
+			elements[0].style.color = "white";
+			this.set('disk_temp',elements[0].value);
+			}
 		else{
 			for (var i = 0; i < length; i++) {
 				elements[i].style.color = "initial";
