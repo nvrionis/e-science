edba8c98422c028b092bfe62cf0b1c6390886956
nvrionis <nick.vrionis@gmail.com>
2015-06-15 16:31:23 +0300

    remove change for enter server ip in cli

--- orka/orka/orka.py
+++ orka/orka/orka.py
@@ -84,13 +84,13 @@ class _ArgCheck(object):
             raise ArgumentTypeError(" %s must containt at least one letter." % val)
 
 
-def task_message(task_id, escience_token, server_url, wait_timer, task='not_progress_bar'):
+def task_message(task_id, escience_token, wait_timer, task='not_progress_bar'):
     """
     Function to check create and destroy celery tasks running from orka-CLI
     and log task state messages.
     """
     payload = {"job": {"task_id": task_id}}
-    yarn_cluster_logger = ClusterRequest(escience_token, server_url, payload, action='job')
+    yarn_cluster_logger = ClusterRequest(escience_token, payload, action='job')
     previous_response = {'job': {'state': 'placeholder'}}
     response = yarn_cluster_logger.retrieve()
     while 'state' in response['job']:
@@ -122,7 +122,6 @@ class HadoopCluster(object):
         self.opts = opts
         try: 
             self.escience_token = authenticate_escience(self.opts['token'])
-            self.server_url = self.opts['server_url']
         except ConnectionError:
             logging.error(' e-science server unreachable or down.')
             exit(error_fatal)
@@ -141,16 +140,14 @@ class HadoopCluster(object):
                                         "ram_slaves": self.opts['ram_slave'], "disk_slaves": self.opts['disk_slave'],
                                         "disk_template": self.opts['disk_template'], "os_choice": self.opts['image'],
                                         "replication_factor": self.opts['replication_factor'], "dfs_blocksize": self.opts['dfs_blocksize']}}
-            yarn_cluster_req = ClusterRequest(self.escience_token, self.server_url, payload, action='cluster')
-            print 1
+            yarn_cluster_req = ClusterRequest(self.escience_token, payload, action='cluster')
             response = yarn_cluster_req.create_cluster()
-            print 2
             if 'task_id' in response['clusterchoice']:
                 task_id = response['clusterchoice']['task_id']
             else:
                 logging.error(response['clusterchoice']['message'])
                 exit(error_fatal)
-            result = task_message(task_id, self.escience_token, self.server_url, wait_timer_create)
+            result = task_message(task_id, self.escience_token, wait_timer_create)
             logging.log(SUMMARY, " Yarn Cluster is active.You can access it through " +
                         result['master_IP'] + ":8088/cluster")
             logging.log(SUMMARY, " The root password of your master VM is " + result['master_VM_password'])
@@ -164,7 +161,7 @@ class HadoopCluster(object):
 
     def destroy(self):
         """ Method for deleting Hadoop clusters in~okeanos."""
-        clusters = get_user_clusters(self.opts['token'], self.opts['server_url'])
+        clusters = get_user_clusters(self.opts['token'])
         for cluster in clusters:
             if (cluster['id'] == self.opts['cluster_id']) and cluster['cluster_status'] == const_cluster_status_active:
                 break
@@ -173,10 +170,10 @@ class HadoopCluster(object):
             exit(error_fatal)
         try:
             payload = {"clusterchoice":{"id": self.opts['cluster_id']}}
-            yarn_cluster_req = ClusterRequest(self.escience_token, self.server_url, payload, action='cluster')
+            yarn_cluster_req = ClusterRequest(self.escience_token, payload, action='cluster')
             response = yarn_cluster_req.delete_cluster()
             task_id = response['clusterchoice']['task_id']
-            result = task_message(task_id, self.escience_token, self.server_url, wait_timer_delete)
+            result = task_message(task_id, self.escience_token, wait_timer_delete)
             logging.log(SUMMARY, ' Cluster with name "%s" and all its resources deleted' %(result))
             stdout.write("DESTROYED {0}".format(result))
         except Exception, e:
@@ -206,10 +203,10 @@ class HadoopCluster(object):
                 exit(error_fatal)
         try:
             payload = {"clusterchoice":{"id": self.opts['cluster_id'], "hadoop_status": action}}
-            yarn_cluster_req = ClusterRequest(self.escience_token, self.server_url, payload, action='cluster')
+            yarn_cluster_req = ClusterRequest(self.escience_token, payload, action='cluster')
             response = yarn_cluster_req.create_cluster()
             task_id = response['clusterchoice']['task_id']
-            result = task_message(task_id, self.escience_token, self.server_url, wait_timer_delete)
+            result = task_message(task_id, self.escience_token, wait_timer_delete)
             logging.log(SUMMARY, result)
             stdout.write("{0}: {1}".format(str.upper(action),result))
         except Exception, e:
@@ -225,7 +222,7 @@ class HadoopCluster(object):
         if opt_filelist == True:
             self.list_pithos_files()
         else:
-            clusters = get_user_clusters(self.opts['token'], self.opts['server_url'])
+            clusters = get_user_clusters(self.opts['token'])
             active_cluster = None
             for cluster in clusters:
                 if (cluster['id'] == self.opts['cluster_id']):
@@ -400,7 +397,7 @@ class HadoopCluster(object):
                                         "dest": "\'{0}\'".format(self.opts['destination']), "user": self.opts['user'],
                                         "password": self.opts['password']}}
 
-        yarn_cluster_req = ClusterRequest(self.escience_token, self.server_url, payload, action='hdfs')
+        yarn_cluster_req = ClusterRequest(self.escience_token, payload, action='hdfs')
         response = yarn_cluster_req.post()
         if 'task_id' in response['hdfs']:
             task_id = response['hdfs']['task_id']
@@ -408,7 +405,7 @@ class HadoopCluster(object):
             logging.error(response['hdfs']['message'])
             exit(error_fatal)
         logging.log(SUMMARY, ' Starting file transfer')
-        result = task_message(task_id, self.escience_token, self.server_url, wait_timer_delete,
+        result = task_message(task_id, self.escience_token, wait_timer_delete,
                                   task='has_progress_bar')
         if result == 0:
             stdout.flush()
@@ -517,7 +514,7 @@ class UserClusterInfo(object):
     
     def list(self):
         try:
-            self.data.extend(get_user_clusters(self.opts['token'], self.opts['server_url']))
+            self.data.extend(get_user_clusters(self.opts['token']))
         except ClientError, e:
             logging.error(e.message)
             exit(error_fatal)
@@ -578,7 +575,6 @@ def main():
                                 datefmt='%H:%M:%S')
     try:
         kamaki_token = get_from_kamaki_conf('cloud "~okeanos"', 'token')
-        kamaki_base_url = get_from_kamaki_conf('orka','base_url')
     except ClientError, e:
         kamaki_token = ' '
         logging.warning(e.message)
@@ -593,8 +589,6 @@ def main():
     common_parser.add_argument("--auth_url", metavar='auth_url', default=auth_url,
                               help='Synnefo authentication url. Default is ' +
                               auth_url)
-    common_parser.add_argument("--server_url", metavar='server_url', default=kamaki_base_url,
-                              help='Url for web application server. Default read from .kamakirc')
     # cluster actions group
     parser_create = orka_subparsers.add_parser('create', parents=[common_parser],
                                      help='Create a Hadoop-Yarn cluster'
