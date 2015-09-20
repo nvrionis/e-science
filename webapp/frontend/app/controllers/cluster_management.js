c064ee53b8c838a4d2d689c4144ea4775625d895
nvrionis <nick.vrionis@gmail.com>
2015-09-20 22:26:27 +0300

    Create dsl

--- webapp/frontend/app/controllers/cluster_management.js
+++ webapp/frontend/app/controllers/cluster_management.js
@@ -1,10 +1,11 @@
 safestr = Ember.Handlebars.SafeString;
 App.ClusterManagementController = Ember.Controller.extend({
 	
-	needs : ['clusterCreate','helpImages'],
+	needs : ['clusterCreate','helpImages','userWelcome'],
 	hue_login_message : '',
 	hue_message : '',
     count : 0,
+    file_count : 0,
     initial_timer_active : function(){
         return this.get('count')>0;
     }.property('count'),
@@ -38,7 +39,18 @@ App.ClusterManagementController = Ember.Controller.extend({
             return tabs_object;
         }
         return tabs_object;
-    }.property(),	
+    }.property(),
+    state_message : function() {
+        var stat_message = this.get('content.state');
+        if (!Ember.isBlank(stat_message)) {
+            var cluster_name = this.get('content.cluster_name');
+            var msg = {
+                'msg_type' : 'warning',
+                'msg_text' : cluster_name +': ' + stat_message
+            };
+            this.controllerFor('userWelcome').send('addMessage', msg);
+        }
+    }.observes('content.state'),
 	cluster_slaves_newsize_static : null,
 	cluster_slaves_newsize : function(key, value){
 	    if (arguments.length > 1){//setter
@@ -102,20 +114,41 @@ App.ClusterManagementController = Ember.Controller.extend({
 	}.property(),
 	actions : {
 	    dsl_create : function(){
+	    var self = this;
             var store = this.get('store');
             var model = this.get('content');
             var cluster_id = model.get('id');
+            var dsl_name = this.get('dsl_filename');
+            var pithos_path = this.get('dsl_pithos_path');
             // unload cached records
-            store.unloadAll('clusterchoice');
-            store.push('clusterchoice',{
-                    'id': 1,
-                    'cluster_edit': cluster_id,
-                }).save();
+            store.unloadAll('dsl');
+            store.fetch('user',1).then(function(user){
+                //success
+	            var response = store.createRecord('dsl',{
+		                    'id': 1,
+		                    'dsl_name': dsl_name,
+		                    'pithos_path': pithos_path,
+		                    'cluster_id': cluster_id,
+		                }).save();
+	            response.then(function(data){
+	                    var msg = {'msg_type':'success','msg_text':'File(%@) with cluster metadata has transferred to pithos container'.fmt(self.get('file_count'))};
+	                    self.get('controllers.userWelcome').send('addMessage',msg);
+	                },function(reason){
+	                	var msg = {'msg_type':'danger','msg_text':'Failed to transfer file(%@) with cluster metadata to pithos container'.fmt(self.get('file_count'))};
+	                	self.get('controllers.userWelcome').send('addMessage',msg);
+	                });
+	            self.set('file_count', self.get('file_count')+1);
+	            self.get('controllers.userWelcome').send('setActiveTab','dsls');
+				self.transitionToRoute('user.welcome');
+			},function(reason){
+                //error
+                console.log(reason);
+            });
 	    },
 	    dsl_filename_default : function(){
 	        var model = this.get('content');
-	        var date_now = new safestr(moment(Date.now()).format('YYYY-MM-DD-HH-mm-ss'))['string'];
-	        var default_filename = "%@-%@-%@-%@".fmt(model.get('cluster_name_noprefix'),model.get('id'),date_now,'cluster-metadata');
+	        var date_now = new safestr(moment(Date.now()).format('YYYY-MM-DD_HH-mm-ss'))['string'];
+	        var default_filename = "%@_%@_%@-%@".fmt(model.get('cluster_name_noprefix'),model.get('id'),date_now,'cluster-metadata');
 	        this.set('dsl_filename',default_filename);
 	    },
 	    dsl_pithospath_default : function(){
