c45dcd8be2e155804b0581f3b386b02cdc4ac148
nvrionis <nick.vrionis@gmail.com>
2015-10-06 18:12:24 +0300

    Administrative refactoring

--- deploy/deploy_project.sh
+++ deploy/deploy_project.sh
@@ -59,7 +59,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 apt-get install -y git python python-dev python-pip;
 pip install ansible==1.9.2;
 pip install kamaki==0.13.1;
-pip install django_admin_bootstrapped==2.4.0;
+pip install django-admin-bootstrapped==2.4.0;
 pip install django-rest-swagger==0.3.2;
 exit'
 echo "Information" > $SERVERNAME.txt
